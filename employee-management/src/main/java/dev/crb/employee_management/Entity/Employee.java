9f10472cea772d0dc40eda507d65b95a2bf4a805
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-07-01 19:00:20 +0530

    Created an Entity (Employee.java)

--- employee-management/src/main/java/dev/crb/employee_management/Entity/Employee.java
+++ employee-management/src/main/java/dev/crb/employee_management/Entity/Employee.java
@@ -1,4 +1,89 @@
 package dev.crb.employee_management.Entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+/*
+The @Entity annotation is used to mark a class as a JPA entity.
+This means that the class is mapped to a table in a database.
+When the JPA provider (such as Hibernate) scans your code,
+it will recognize this class as a representation of a database table.
+*/
+@Entity
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String position;
+    private double salary;
+
+    public Employee()
+    {
+
+    }
+
+    public Employee(Long id, String firstName, String lastName, String email, String position, double salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
