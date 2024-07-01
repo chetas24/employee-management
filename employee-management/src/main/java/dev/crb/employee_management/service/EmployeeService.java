e0fd3c7e4deb5ccca159cf6c5e248441dfc6e89f
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-07-01 19:39:09 +0530

    updated

--- employee-management/src/main/java/dev/crb/employee_management/service/EmployeeService.java
+++ employee-management/src/main/java/dev/crb/employee_management/service/EmployeeService.java
@@ -1,4 +1,36 @@
 package dev.crb.employee_management.service;
 
+import dev.crb.employee_management.Entity.Employee;
+import dev.crb.employee_management.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
 }
+
