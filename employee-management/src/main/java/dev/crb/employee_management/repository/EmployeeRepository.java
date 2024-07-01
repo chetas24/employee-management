b476d7bc7f952a300e7c11c8a788d560e43a99c9
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-07-01 19:14:23 +0530

    Created an Repository (EmployeeRepository.java)

--- employee-management/src/main/java/dev/crb/employee_management/repository/EmployeeRepository.java
+++ employee-management/src/main/java/dev/crb/employee_management/repository/EmployeeRepository.java
@@ -1,4 +1,10 @@
 package dev.crb.employee_management.repository;
 
-public class EmployeeRepository {
+import dev.crb.employee_management.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>  {
+
 }
