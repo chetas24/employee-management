995e5da102f122abd529e6e55b08666f0a0bf37a
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-07-03 17:00:42 +0530

    updated Test class

--- employee-management/src/test/java/dev/crb/employee_management/EmployeeManagementApplicationTests.java
+++ employee-management/src/test/java/dev/crb/employee_management/EmployeeManagementApplicationTests.java
@@ -1,13 +1,24 @@
 package dev.crb.employee_management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class EmployeeManagementApplicationTests {
+@AutoConfigureMockMvc
+public class EmployeeManagementApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void shouldReturnDefaultMessage() throws Exception {
+		mockMvc.perform(get("/employees"))
+				.andExpect(status().isOk());
 	}
-
 }
